2c5893bcf734757528961d9e154e57ad0bcf0fa7
OG'ABEK <shomoyxhacker007@mail.ru>
2022-07-26 07:24:34 -0500

    some changes

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -16,15 +16,10 @@ export default {
 </script>
 
 <template>
-  <button
-    class="w-full bg-gray-200 rounded-lg p-4 transition md:text-lg md:p-6"
-    @click="store.checkAnswer(text)"
-    v-html="text"
-    :disabled="store.showAnswer"
-    :class="{
+  <button class="w-full p-4 transition bg-gray-200 rounded-lg md:text-lg md:p-6" @click="store.checkAnswer(text)"
+    v-html="text" :disabled="store.showAnswer" :class="{
       'bg-red-200': store.showAnswer && isInvalidAnswer,
       'bg-green-200': store.showAnswer && isValidAnswer,
       'hover:bg-gray-300': !store.showAnswer,
-    }"
-  ></button>
+    }"></button>
 </template>
--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,69 +1,50 @@
 <template>
   <Loader v-if="store.loading"></Loader>
-  <div v-else class="w-11/12 mx-auto grid grid-cols-1 gap-y-8 lg:w-5/12">
-    <h1 class="text-center text-3xl my-12 font-bold">
+  <div v-else class="grid w-11/12 grid-cols-1 mx-auto gap-y-8 lg:w-5/12">
+    <h1 class="my-12 text-3xl font-bold text-center">
       Trivia<span class="text-purple-500">GO</span>
     </h1>
     <Listbox as="div" v-model="selectedCategory">
       <ListboxLabel class="block text-sm font-medium text-gray-700">
         Choose a category
       </ListboxLabel>
-      <div class="mt-1 relative">
+      <div class="relative mt-1">
         <ListboxButton
-          class="relative w-full bg-white border border-gray-300 rounded-md shadow-md pl-3 pr-10 py-4 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
-        >
+          class="relative w-full py-4 pl-3 pr-10 text-left bg-white border border-gray-300 rounded-md shadow-md cursor-default focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 sm:text-sm">
           <span class="flex items-center">
-            <span class="ml-3 block truncate">{{
-              selectedCategory || 'Loading...'
+            <span class="block ml-3 truncate">{{
+                selectedCategory || 'Loading...'
             }}</span>
           </span>
-          <span
-            class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
-          >
-            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <span class="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
+            <SelectorIcon class="w-5 h-5 text-gray-400" aria-hidden="true" />
           </span>
         </ListboxButton>
 
-        <transition
-          leave-active-class="transition ease-in duration-100"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
-        >
+        <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
+          leave-to-class="opacity-0">
           <ListboxOptions
-            class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
-          >
-            <ListboxOption
-              as="template"
-              v-for="category in categories"
-              :key="category.id"
-              :value="category.name"
-              v-slot="{ active, selectedCategory }"
-            >
-              <li
-                :class="[
-                  active ? 'text-white bg-purple-600' : 'text-gray-900',
-                  'cursor-default select-none relative py-2 pl-3 pr-9',
-                ]"
-              >
+            class="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <ListboxOption as="template" v-for="category in categories" :key="category.id" :value="category.name"
+              v-slot="{ active, selectedCategory }">
+              <li :class="[
+                active ? 'text-white bg-purple-600' : 'text-gray-900',
+                'cursor-default select-none relative py-2 pl-3 pr-9',
+              ]">
                 <div class="flex items-center">
-                  <span
-                    :class="[
-                      selectedCategory ? 'font-semibold' : 'font-normal',
-                      'ml-3 block truncate',
-                    ]"
-                  >
+                  <span :class="[
+                    selectedCategory ? 'font-semibold' : 'font-normal',
+                    'ml-3 block truncate',
+                  ]">
                     {{ category.name }}
                   </span>
                 </div>
 
-                <span
-                  v-if="selectedCategory"
-                  :class="[
-                    active ? 'text-white' : 'text-purple-600',
-                    'absolute inset-y-0 right-0 flex items-center pr-4',
-                  ]"
-                >
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                <span v-if="selectedCategory" :class="[
+                  active ? 'text-white' : 'text-purple-600',
+                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                ]">
+                  <CheckIcon class="w-5 h-5" aria-hidden="true" />
                 </span>
               </li>
             </ListboxOption>
@@ -76,60 +57,41 @@
       <ListboxLabel class="block text-sm font-medium text-gray-700">
         Choose a difficulty
       </ListboxLabel>
-      <div class="mt-1 relative">
+      <div class="relative mt-1">
         <ListboxButton
-          class="relative w-full bg-white border border-gray-300 rounded-md shadow-md pl-3 pr-10 py-4 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
-        >
+          class="relative w-full py-4 pl-3 pr-10 text-left bg-white border border-gray-300 rounded-md shadow-md cursor-default focus:outline-none focus:ring-1 focus:ring-purple-500 focus:border-purple-500 sm:text-sm">
           <span class="flex items-center">
-            <span class="ml-3 block truncate">{{ selectedDifficulty }}</span>
+            <span class="block ml-3 truncate">{{ selectedDifficulty }}</span>
           </span>
-          <span
-            class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
-          >
-            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <span class="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
+            <SelectorIcon class="w-5 h-5 text-gray-400" aria-hidden="true" />
           </span>
         </ListboxButton>
 
-        <transition
-          leave-active-class="transition ease-in duration-100"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
-        >
+        <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
+          leave-to-class="opacity-0">
           <ListboxOptions
-            class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
-          >
-            <ListboxOption
-              as="template"
-              v-for="difficulty in difficulties"
-              :key="difficulty"
-              :value="difficulty"
-              v-slot="{ active, selectedDifficulty }"
-            >
-              <li
-                :class="[
-                  active ? 'text-white bg-purple-600' : 'text-gray-900',
-                  'cursor-default select-none relative py-2 pl-3 pr-9',
-                ]"
-              >
+            class="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <ListboxOption as="template" v-for="difficulty in difficulties" :key="difficulty" :value="difficulty"
+              v-slot="{ active, selectedDifficulty }">
+              <li :class="[
+                active ? 'text-white bg-purple-600' : 'text-gray-900',
+                'cursor-default select-none relative py-2 pl-3 pr-9',
+              ]">
                 <div class="flex items-center">
-                  <span
-                    :class="[
-                      selectedDifficulty ? 'font-semibold' : 'font-normal',
-                      'ml-3 block truncate',
-                    ]"
-                  >
+                  <span :class="[
+                    selectedDifficulty ? 'font-semibold' : 'font-normal',
+                    'ml-3 block truncate',
+                  ]">
                     {{ difficulty }}
                   </span>
                 </div>
 
-                <span
-                  v-if="selectedDifficulty"
-                  :class="[
-                    active ? 'text-white' : 'text-purple-600',
-                    'absolute inset-y-0 right-0 flex items-center pr-4',
-                  ]"
-                >
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                <span v-if="selectedDifficulty" :class="[
+                  active ? 'text-white' : 'text-purple-600',
+                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                ]">
+                  <CheckIcon class="w-5 h-5" aria-hidden="true" />
                 </span>
               </li>
             </ListboxOption>
@@ -137,10 +99,8 @@
         </transition>
       </div>
     </Listbox>
-    <button
-      class="px-12 py-4 bg-gray-600 text-white text-lg rounded-lg hover:bg-gray-700 transition w-full"
-      @click="startQuiz()"
-    >
+    <button class="w-full px-12 py-4 text-lg text-white transition bg-gray-600 rounded-lg hover:bg-gray-700"
+      @click="startQuiz()">
       Start quiz
     </button>
   </div>
